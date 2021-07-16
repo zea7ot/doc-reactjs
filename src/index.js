cc3efb2867b401ed26b6f2e6626da23047cb1074
Leon <yanglyu.leon.7@gmail.com>
2021-07-16 10:25:19 -0700

    start: official doc of ReactJS

--- src/index.js
+++ src/index.js
@@ -1,60 +1,60 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
 class Square extends React.Component {
-  render () {
-    return <button className='square'>{/* TODO */}</button>
+  render() {
+    return <button className="square">{/* TODO */}</button>;
   }
 }
 
 class Board extends React.Component {
-  renderSquare (i) {
-    return <Square />
+  renderSquare(i) {
+    return <Square />;
   }
 
-  render () {
-    const status = 'Next player: X'
+  render() {
+    const status = 'Next player: X';
 
     return (
       <div>
-        <div className='status'>{status}</div>
-        <div className='board-row'>
+        <div className="status">{status}</div>
+        <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className='board-row'>
+        <div className="board-row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div className='board-row'>
+        <div className="board-row">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
       </div>
-    )
+    );
   }
 }
 
 class Game extends React.Component {
-  render () {
+  render() {
     return (
-      <div className='game'>
-        <div className='game-board'>
+      <div className="game">
+        <div className="game-board">
           <Board />
         </div>
-        <div className='game-info'>
+        <div className="game-info">
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
-    )
+    );
   }
 }
 
 // ========================================
 
-ReactDOM.render(<Game />, document.getElementById('root'))
+ReactDOM.render(<Game />, document.getElementById('root'));
